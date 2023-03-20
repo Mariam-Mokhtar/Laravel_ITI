9c9352cfc1083f60ef687e71d57a6ee337f7d2ad
Mariam Reda <mariamreda1819@gmail.com>
2023-03-20 12:30:40 +0200

    edit lab2

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,25 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
     public function store(Request $request)
-    {   Comment::create([
-            'comment' => $request->comment,
-            'commentable_id' => $request->post_id,
-            'commentable_type' =>"App\Models\Post",
-            'user_id' => $request->creator,
-        ]);
+    {           
+        $post=Post::find($request->post_id);
+        $post->comment()->create($request->all());
         return redirect()->back();
     }
     public function destroy($id)
     {
         if(is_numeric($id))
         {
-        Comment::where("id", $id)->delete();
-        return redirect()->back();
+          $comment=Comment::find($id);
+          $post_id=$comment->commentable->id;
+          $comment->delete();
+          return to_route('posts.show',$post_id );
         }
     }
     public function update(Request $request,$id)
